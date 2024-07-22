18024ba8e5f6705c22c64922c4f5b9d403e455fc
kohai-fred <frederick.vu.pro@gmail.com>
2024-07-22 08:22:27 +0200

    update: question-1.3 -  use pivot table

--- dto/byYear.py
+++ dto/byYear.py
@@ -1,5 +1,8 @@
 import json
+import pandas as pd
 from ..extract.all_names import names
+
+total_birth_by_year_and_sex = pd.pivot_table(names, values='birth', index='year', columns='sex', aggfunc='sum', fill_value=0)
 
 def namesByYear(year):
   return names[names["year"] == year]
@@ -8,16 +11,17 @@ def get_names_by_year(year):
   result = {}
   json_str = namesByYear(year).to_json(orient="records")
   result['data'] = json.loads(json_str)
-  result["year"] = year
   return result
 
 def get_sum_by_year_and_sex(year):
-  result = namesByYear(year).groupby('sex')['birth'].sum().to_dict()
+  result = total_birth_by_year_and_sex.loc[year].to_dict()
   result["year"] = year
   return result
 
 def get_total_by_sex():
-  result= names.groupby('sex')['birth'].sum().to_dict()
+  result = {}
+  result["data"] = json.loads(total_birth_by_year_and_sex.to_json(orient="index" ))
+  result["total"]= names.groupby('sex')['birth'].sum().to_dict()
   result["year"] = {
     "start": int(names["year"].min()),
     "end": int(names["year"].max()),
