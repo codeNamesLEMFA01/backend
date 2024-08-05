fbae090e1cee59d9f040490bc7987f52985f14fd
Eolran <66948283+Eolran@users.noreply.github.com>
2024-08-05 16:15:07 +0200

    poc auth system with FastAPI integrated OAuth2

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ from .routers.details import router as details_router
 from .cron.cron import router as cron_router
 from .database import connect_to_mongo
 import logging
+import argparse
+from mongoengine import connect
+from .routers.auth import router as auth_router
+# from .routers.posts import router as posts_router
 
 
 app = FastAPI()
@@ -38,3 +42,15 @@ async def startup_event():
 app.include_router(names_router)
 app.include_router(details_router)
 app.include_router(cron_router)
+app.include_router(seed_router)
+app.include_router(yopByYear)
+
+app.include_router(auth_router)
+# app.include_router(posts_router)
+
+def main() -> None:
+    uvicorn.run("main:app", reload=True)
+
+
+if __name__ == "__main__":
+    main()
