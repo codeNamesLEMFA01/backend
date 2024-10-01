3058e2cf7c47b9f593044d0d1063af2718c1c7ac
Eolran <66948283+Eolran@users.noreply.github.com>
2024-10-01 10:10:52 +0200

    quick fix token expire time

--- app/auth/authServices.py
+++ app/auth/authServices.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))*60
 
 def setCookie(response, value):
     response.set_cookie(key="code_names_access_token", value=value, secure=True, samesite="None", max_age=ACCESS_TOKEN_EXPIRE_MINUTES)
